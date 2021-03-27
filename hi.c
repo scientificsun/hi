c7c76d961aad4c39d42b87f010fe00a09b57138e
Alan Knight <alan.knight@me.com>
2021-03-27 21:52:46 +0000

    new strings

--- hi.c
+++ hi.c
@@ -2,9 +2,12 @@
 
 int main(int argc, char **argv)
 {
+	char* argument = "Argument";
+	char* arguments = "Arguments";
+
     printf("Hello, World!\n");
 	if(argc > 1)
 	{
-		printf("%d variable passed on command line\n", argc -1);
+		printf("%d %S passed on command line\n", argc -1, argc>2 ? arguments : argument);
 	}
 }
