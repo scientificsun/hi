4179f2ea9fd9546e4ea972200be588cc7086431b
Alan Knight <alan.knight@me.com>
2021-04-01 22:16:35 +0100

    added arguments processing

--- hi.c
+++ hi.c
@@ -2,9 +2,14 @@
 
 int main(int argc, char **argv)
 {
+	int 	i;
     printf("Hello, World!\n");
 	if(argc > 1)
 	{
 		printf("%d %s passed on command line\n", argc -1, argc>2 ? "Arguments" : "Argument");
+	}
+	for(i=1; i<argc; i++)
+	{
+		printf("%s\n", argv[i]);
 	}
 }
